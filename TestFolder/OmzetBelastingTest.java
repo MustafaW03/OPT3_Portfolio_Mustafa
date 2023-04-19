3d3d6a65db599ff79da0860d6f5b64ab144f1ae5
MustafaW <mustafawarsame03@gmail.com>
2023-04-19 17:36:23 +0200

    test beetje aangepast

--- TestFolder/OmzetBelastingTest.java
+++ TestFolder/OmzetBelastingTest.java
@@ -26,28 +26,46 @@ public class OmzetBelastingTest {
 
     @Test
     public void testBerekenBelasting() {
+
+        //Arrange
         OmzetBelasting omzetBelasting = new OmzetBelasting();
+
+        //Act
         int expected = (int) (gegevens.getUurloon() * gegevens.getGewerkteUren() * 0.21);
         int actual = omzetBelasting.BerekenBelasting();
+
+        //Assert
         assertEquals(expected, actual);
     }
 
     @Test
     public void testBerekenVoorBelasting() {
+
+        //Arrange
         OmzetBelasting omzetBelasting = new OmzetBelasting();
+
+        //Act
         int expected = (int) (gegevens.getKosten() * 0.21);
         int actual = omzetBelasting.BerekenVoorBelasting();
+
+        //Assert
         assertEquals(expected, actual);
     }
 
    
 
     public void testToonBelasting() {
+
+        //Arrange
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         OmzetBelasting omzetBelasting = new OmzetBelasting();
+
+        //Act
         omzetBelasting.toonBelasting();
         String expectedOutput = "\nDe omzetbelasting voor deze klus ziet er als volgt uit: \nUurloon: 15 EUR\nGewerkte uren: 20 UUR\nOmzet: 300 EUR\nVoorbelasting: 21 EUR\nOmzetbelasting: 63 EUR\n\nWilt uw dit overzicht printen naar een extern bestand? \n";
+
+        //Assert
         assertEquals(expectedOutput, outContent.toString());
     }
 }
